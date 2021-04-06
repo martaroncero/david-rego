d1aca90223953643c84ae8baaf0d378a62abad81
Marta Roncero <m.ronceroolmeda@gmail.com>
2021-04-06 12:57:14 +0100

    Changed main id

--- js/fade.js
+++ js/fade.js
@@ -1,1 +1,13 @@
-fade.js
+
+
+// TODO: React to a click on the button!
+const element = document.querySelector('#main');
+
+
+ element.classList.add("fade-out");
+
+ window.addEventListener('DOMContentLoaded', (event) => {
+  element.classList.remove("fade-out");
+    });
+
+
